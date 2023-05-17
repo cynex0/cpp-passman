3befaac7b0ab770c5e689f37b185679f6429537f
unknown <m.orlov10@gmail.com>
2023-05-17 22:53:41 +0300

    add class declarations

---
+++ user.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Max on 17/05/2023.
+//
+
+#include "user.h"
---
+++ vault.h
@@ -0,0 +1,8 @@
+//
+// Created by Max on 17/05/2023.
+//
+
+#ifndef CPP_PASSMAN_VAULT_H
+#define CPP_PASSMAN_VAULT_H
+
+#endif //CPP_PASSMAN_VAULT_H
