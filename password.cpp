12e75dc2b21bb2dbcda0bc7c3b7aa5479e80ebee
unknown <m.orlov10@gmail.com>
2023-05-22 17:35:08 +0300

    add password class

---
+++ password.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Max on 18/05/2023.
+//
+
+#include "password.h"
---
+++ password.h
@@ -0,0 +1,8 @@
+//
+// Created by Max on 18/05/2023.
+//
+
+#ifndef CPP_PASSMAN_PASSWORD_H
+#define CPP_PASSMAN_PASSWORD_H
+
+#endif //CPP_PASSMAN_PASSWORD_H
